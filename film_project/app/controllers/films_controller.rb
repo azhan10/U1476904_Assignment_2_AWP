ab2159a25b60482a80b5f5e1c838cdd32f3090ba
azhan10 <rashid.azhan6@hotmail.co.uk>
2017-03-20 17:09:53 +0000

    PDF Feature
    The project now contains the feature to export film information into a
    PDF format.

--- film_project/app/controllers/films_controller.rb
+++ film_project/app/controllers/films_controller.rb
@@ -39,6 +39,55 @@ class FilmsController < ApplicationController
        flash[:anyReviewAverage] = ['Average Score: ', @reviewAverage].join()
     end
 
+    respond_to do |format|
+    format.html
+    format.pdf do
+      pdf = Prawn::Document.new
+      pdf.text @film.filmtitle, :size => 30, :align => :center
+      pdf.text 'Film Title: ' + @film.filmtitle
+      pdf.move_down 10
+      pdf.text ['Film Description: ', @film.filmdescription].join();
+      pdf.move_down 10
+      pdf.text ['Film Star Name: ', @film.filmstarname].join();
+      pdf.move_down 10
+      pdf.text ['Film Rating: ', @film.filmrating].join();
+      pdf.move_down 10
+      pdf.text ['Film Director: ', @film.filmdirector].join();
+      pdf.move_down 10
+
+      if @reviewAmount == 0
+        pdf.text "No Reviews", :align => :center, :size => 25
+      else
+        pdf.text ['Number of reviews: ', @reviewAmount].join()
+        pdf.move_down 10
+        pdf.text ['Average Rating: ', @reviewAverage].join()
+
+        pdf.text "Reviews", :style => :bold, :align => :center, :size => 25
+
+        currentReviews = [["Customer Name", "Film Rating", "Comment", "Review Added"]]
+        currentReviews += @film.filmreviews.map do |s|
+
+          [
+            s.commenter,
+            s.rating,
+            s.body,
+            s.created_at
+          ]
+        end
+
+        pdf.table(currentReviews, header: true, width: 400, :row_colors => ["F0f0f0", "ffffcc"]) do
+          row(0).font_style = :bold
+        end
+      end
+
+
+      send_data pdf.render,
+          filename: "film_#{@film.filmtitle}",
+          type: 'application/pdf',
+          disposition: 'inline'
+      end
+  end
+
   end
 
   private
